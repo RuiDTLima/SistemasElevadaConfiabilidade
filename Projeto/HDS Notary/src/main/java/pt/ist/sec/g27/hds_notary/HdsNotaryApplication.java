d0be37ee66fb46ebdb53a3141c0b9de82486cd29
nveloso <nveloso21@gmail.com>
2019-03-14 16:10:28 +0000

    Add gitignore and start project

---
+++ Projeto/HDS Notary/src/main/java/pt/ist/sec/g27/hds_notary/HdsNotaryApplication.java
@@ -0,0 +1,13 @@
+package pt.ist.sec.g27.hds_notary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdsNotaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdsNotaryApplication.class, args);
+    }
+
+}
---
+++ Projeto/HDS Notary/src/test/java/pt/ist/sec/g27/hds_notary/HdsNotaryApplicationTests.java
@@ -0,0 +1,16 @@
+package pt.ist.sec.g27.hds_notary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HdsNotaryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
