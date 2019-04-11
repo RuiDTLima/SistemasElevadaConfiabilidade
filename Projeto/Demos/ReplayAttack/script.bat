52a21a08c268e2f39603805c5ecae1611823b795
Ricardo <ricagraca@hotmail.com>
2019-04-11 20:34:35 +0100

    Fixed id for jar execution

--- Projeto/Demos/ReplayAttack/script.bat
+++ Projeto/Demos/ReplayAttack/script.bat
@@ -4,4 +4,4 @@ start cmd /k java -jar hds_notary-0.0.1-SNAPSHOT.jar --server.port=8080
 cd ../alice
 start cmd /k java -jar alice.jar 1 --server.port=8081
 cd ../eve
-start cmd /k java -jar eve.jar 2 --server.port=8083
+start cmd /k java -jar eve.jar 3 --server.port=8083
