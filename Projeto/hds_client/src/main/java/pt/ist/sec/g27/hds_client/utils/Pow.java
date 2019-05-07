c2043d0c74b197bb46c30066e2c84495cf2bf506
nveloso <nveloso21@gmail.com>
2019-05-07 22:37:55 +0100

    Refactor Pow class

--- Projeto/hds_client/src/main/java/pt/ist/sec/g27/hds_client/utils/Pow.java
+++ Projeto/hds_client/src/main/java/pt/ist/sec/g27/hds_client/utils/Pow.java
@@ -1,4 +1,3 @@
-
 package pt.ist.sec.g27.hds_client.utils;
 
 import java.io.ByteArrayOutputStream;
@@ -7,93 +6,68 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.lang.Math;
 
 // Proof of work
 public class Pow {
+    private static final String SHA256 = "SHA-256";
+    private static final int BYTE_SIZE = 8; // 8 BIT
 
-    private final String SHA256 = "SHA-256";
-    private final int BYTE_SIZE = 8; // 8 BIT
     private byte[] message;
-    private int number_bit;
+    private int numberBit;
+    private final MessageDigest digest;
 
-    Pow(String message, int number_bit) throws Exception {
-        if( number_bit > 256 ) { throw new Exception("Number of bits must be at max 256 bit"); }
-        this.message = message.getBytes(StandardCharsets.UTF_8);
-        this.number_bit = number_bit;
-    }
-
-    Pow(byte[] message, int number_bit) throws Exception {
-        if( number_bit > 256 ) { throw new Exception("Number of bits must be at max 256 bit"); }
+    Pow(byte[] message, int numberBit) throws NoSuchAlgorithmException {
+        if (numberBit > 256)
+            throw new IllegalArgumentException("Number of bits must be at max 256 bit.");
+        this.digest = MessageDigest.getInstance(SHA256);
         this.message = message;
-        this.number_bit = number_bit;
+        this.numberBit = numberBit;
     }
 
-    public BigInteger compute(){
+    Pow(String message, int numberBit) throws NoSuchAlgorithmException {
+        this(message.getBytes(StandardCharsets.UTF_8), numberBit);
+    }
 
-        BigInteger i= BigInteger.ZERO;
+    public BigInteger compute() {
+        BigInteger i = BigInteger.ZERO;
 
-        while(true)
-        {
-            try
-            {
-                boolean satisfies = verify(i);
-                if(satisfies){return i;}
-                i = i.add(BigInteger.ONE);
-            }catch(NoSuchAlgorithmException e)
-            {
-                System.out.println("Erro: NoSuchAlgorithmException " + e.getMessage());
-            }
+        while (true) {
+            if (verify(i))
+                return i;
+            i = i.add(BigInteger.ONE);
         }
-
     }
 
-    private boolean verify(BigInteger i) throws NoSuchAlgorithmException
-    {
+    private boolean verify(BigInteger i) {
+        byte[] concatMessageI = concat(message, i.toByteArray());
+        byte[] encodedHash = digest(concatMessageI);
+        int numberOfByte = (int) Math.ceil(numberBit / 8.0);
 
-        MessageDigest digest = MessageDigest.getInstance(SHA256);
-        byte[] concat_message_i = concat(message,i.toByteArray());
-        byte[] encodedhash = digest.digest(concat_message_i);
-        int number_of_byte = (int) Math.ceil(number_bit/8.0);
-
-
-        for(int j=0 ; j<number_of_byte; j++)
-        {
-            for(int t=0 ; t<BYTE_SIZE ; t ++)
-            {
-                int bit = (encodedhash[j] >> t) & 1;
-                if( j*BYTE_SIZE + t >= number_bit ){
+        for (int j = 0; j < numberOfByte; j++) {
+            for (int t = 0; t < BYTE_SIZE; t++) {
+                int bit = (encodedHash[j] >> t) & 1;
+                if (j * BYTE_SIZE + t >= numberBit)
                     break;
-                }
-                if(bit == 1){
+                if (bit == 1)
                     return false;
-                }
             }
         }
-
         return true;
-
     }
 
-    private byte[] concat(byte[] a, byte[] b)
-    {
-
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-        try
-        {
+    private byte[] concat(byte[] a, byte[] b) {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
             outputStream.write(a);
             outputStream.write(b);
-            byte c[] = outputStream.toByteArray();
-            return c;
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        catch(IOException e)
-        {
-            System.out.println(e.getMessage());
-        }
-
-        return null;
-
     }
 
+    private byte[] digest(byte[] input) {
+        byte[] hash = digest.digest(input);
+        digest.reset();
+        return hash;
+    }
 }
